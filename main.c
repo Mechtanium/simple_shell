43bb6436c552dc3b7e5e1975f05ea07d12de06c4
Enosolease M. Ame <enosmath@gmail.com>
2022-11-16 04:33:48 -0800

    bug fix

--- main.c
+++ main.c
@@ -11,13 +11,29 @@
  */
 int main(int ac, char **av, char **env)
 {
+	int s, ch, t = 0;
+	char **arr;
+
 	(void) ac;
 	(void) av;
+
+	ch = fork();
+
+	if (ch == 0)
+	{
+		arr = malloc(sizeof(char *));
+		_strcpy(arr[0], "./test", 8);
+		s = execve(arr[0], arr, &t);
+	}
+	else
+	{
+		wait(&t);
+/*get response child prpcess*/
 	if (isatty(1) == 1)
 		interactive(env);
 	else
 		non_interactive(env);
-
+	}
 	return (EXIT_SUCCESS);
 }
 
