0881dab721e82e12207135a0dbff36ebde3363c2
Ade-ojo Tolulope <tolexskii@gmail.com>
2022-11-17 01:49:59 -0800

    Revert "final commit"
    This reverts commit 48154e653b7449072da6c8d0aa79387079506d69.

--- main.h
+++ main.h
@@ -9,12 +9,6 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <sys/stat.h>
-#include <stdarg.h>
-#include <time.h>
-#include <stdbool.h>
-
-extern char **environ;
-extern __sighandler_t signal(int __sig, __sighandler_t __handler);
 
 /**
  * _putchar - Don't delete _putchar, its used for testing
@@ -33,40 +27,4 @@ char *to_exe(char *cmd);
 void _realloc(char ***cmd_a, size_t *n);
 void _exec(char *cmd, char *temp, char **cmd_a, size_t n, int c, char **env);
 void builtin_env(char **env);
-
-
-void execute(char *cmde, char **cmd); 
-int checks(char **cmd, char *buf);
-void prompt(void);
-void signal_handle( int n);
-char **tokens(char *line);
-char *test_path(char **path, char *cmd);
-char *append_path(char *path, char *cmd);
-int handle_builtin(char **cmd, char *line);
-void exit_cmd(char **cmd, char *line);
-void print_env(void);
-int _strlen(char *str);
-int _strncmp(char *str1, char *str2, int n);
-char *_strdup(char *str);
-char *_strchr(char *str, char c);
-char *find_path(void);
-void free_buffers(char **buf);
-
-struct builtin
-{
-	char *env;
-	char *exit;
-} builtin;
-
-struct info
-{
-	int final_exit;
-	int ln_count;
-} info;
-
-struct flags
-{
-	bool interactive;
-} flags;
-
 #endif
